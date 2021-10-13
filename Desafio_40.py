a6e0ed133cab5bab3d95662e54a15d83b3115996
Vlop01 <92225865+Vlop01@users.noreply.github.com>
2021-10-13 20:09:40 -0300

    Update Desafio_40.py
    Consertado o português do comentário

--- Desafio_40.py
+++ Desafio_40.py
@@ -1,4 +1,4 @@
-#Crie um programa que leia duas noras de um launo e calcule sua média, mostrando uma mensagem no final, de acordo com a
+#Crie um programa que leia duas notas de um aluno e calcule sua média, mostrando uma mensagem no final, de acordo com a
 #média atingida
 #-Média abaixo de 5.0: reprovado
 #-Média entre 5.0 e 6.9: recuperação
